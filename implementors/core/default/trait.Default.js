a4defe5f748b7cc72e4bbeeecee77b96b8e365a1
chriamue <chriamue@users.noreply.github.com>
2022-12-28 16:45:04 +0000

    deploy: 4e6e98dff3c2f9f961510fa499668472b57efb0c

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"object_detector_rust":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"object_detector_rust/dataset/struct.DummyDataSet.html\" title=\"struct object_detector_rust::dataset::DummyDataSet\">DummyDataSet</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
