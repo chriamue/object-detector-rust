ad700518a19851042275772efc9afcac67ad5849
Christian M <chriamue@gmail.com>
2022-12-28 10:49:09 +0100

    :sparkles: added bounding box

---
+++ src/bbox.rs
@@ -0,0 +1,273 @@
+/// Struct for representing a bounding box
+pub struct BBox {
+    /// X coordinate of the top-left corner of the bounding box
+    pub x: i32,
+    /// Y coordinate of the top-left corner of the bounding box
+    pub y: i32,
+    /// Width of the bounding box
+    pub width: i32,
+    /// Height of the bounding box
+    pub height: i32,
+}
+
+impl BBox {
+    /// Returns the left coordinate of the bounding box
+    pub fn left(&self) -> i32 {
+        self.x
+    }
+
+    /// Returns the right coordinate of the bounding box
+    pub fn right(&self) -> i32 {
+        self.x + self.width
+    }
+
+    /// Returns the top coordinate of the bounding box
+    pub fn top(&self) -> i32 {
+        self.y
+    }
+
+    /// Returns the bottom coordinate of the bounding box
+    pub fn bottom(&self) -> i32 {
+        self.y + self.height
+    }
+
+    /// Calculates the width of the intersection between two bounding boxes
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use object_detector_rust::BBox;
+    ///
+    /// let bbox1 = BBox {
+    ///     x: 0,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox2 = BBox {
+    ///     x: 50,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox3 = BBox {
+    ///     x: 150,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox4 = BBox {
+    ///     x: -50,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    ///
+    /// assert_eq!(bbox1.intersect_width(&bbox2), 50);
+    /// assert_eq!(bbox1.intersect_width(&bbox3), 0);
+    /// assert_eq!(bbox1.intersect_width(&bbox4), 50);
+    /// assert_eq!(bbox2.intersect_width(&bbox3), 0);
+    /// assert_eq!(bbox2.intersect_width(&bbox4), 0);
+    /// assert_eq!(bbox3.intersect_width(&bbox4), 0);
+    /// ```
+    pub fn intersect_width(&self, other: &BBox) -> i32 {
+        (self.right().min(other.right()) - self.left().max(other.left())).max(0)
+    }
+
+    /// Calculates the height of the intersection between two bounding boxes
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use object_detector_rust::BBox;
+    ///
+    /// let bbox1 = BBox {
+    ///     x: 0,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox2 = BBox {
+    ///     x: 0,
+    ///     y: 50,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox3 = BBox {
+    ///     x: 0,
+    ///     y: 150,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox4 = BBox {
+    ///     x: 0,
+    ///     y: -50,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    ///
+    /// assert_eq!(bbox1.intersect_height(&bbox2), 50);
+    /// assert_eq!(bbox1.intersect_height(&bbox3), 0);
+    /// assert_eq!(bbox1.intersect_height(&bbox4), 50);
+    /// assert_eq!(bbox2.intersect_height(&bbox3), 0);
+    /// assert_eq!(bbox2.intersect_height(&bbox4), 0);
+    /// assert_eq!(bbox3.intersect_height(&bbox4), 0);
+    /// ```
+    pub fn intersect_height(&self, other: &BBox) -> i32 {
+        (self.bottom().min(other.bottom()) - self.top().max(other.top())).max(0)
+    }
+
+    /// Calculates the area of the intersection between two bounding boxes
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use object_detector_rust::BBox;
+    ///
+    /// let bbox1 = BBox {
+    ///     x: 0,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox2 = BBox {
+    ///     x: 50,
+    ///     y: 50,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox3 = BBox {
+    ///     x: 150,
+    ///     y: 150,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    ///
+    /// assert_eq!(bbox1.intersect_area(&bbox2), 2500);
+    /// assert_eq!(bbox1.intersect_area(&bbox3), 0);
+    /// assert_eq!(bbox2.intersect_area(&bbox3), 0);
+    /// ```
+    pub fn intersect_area(&self, other: &BBox) -> i32 {
+        self.intersect_width(other) * self.intersect_height(other)
+    }
+
+    /// Calculates the area of the bounding box
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use object_detector_rust::BBox;
+    ///
+    /// let bbox = BBox {
+    ///     x: 0,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    ///
+    /// assert_eq!(bbox.self_area(), 10000);
+    /// ```
+    pub fn self_area(&self) -> i32 {
+        self.width * self.height
+    }
+
+    /// Calculates the area of the union between two bounding boxes
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use object_detector_rust::BBox;
+    ///
+    /// let bbox1 = BBox {
+    ///     x: 0,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox2 = BBox {
+    ///     x: 50,
+    ///     y: 50,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    ///
+    /// assert_eq!(bbox1.union_area(&bbox2), 17500);
+    /// ```
+    pub fn union_area(&self, other: &BBox) -> i32 {
+        self.self_area() + other.self_area() - self.intersect_area(other)
+    }
+
+    /// Calculates the overlap between two bounding boxes as the ratio of their intersecting area to their union area
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use object_detector_rust::BBox;
+    ///
+    /// let bbox1 = BBox {
+    ///     x: 0,
+    ///     y: 0,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox2 = BBox {
+    ///     x: 50,
+    ///     y: 50,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    /// let bbox3 = BBox {
+    ///     x: 150,
+    ///     y: 150,
+    ///     width: 100,
+    ///     height: 100,
+    /// };
+    ///
+    /// assert!(bbox1.overlap(&bbox2) > 0.14);
+    /// assert_eq!(bbox1.overlap(&bbox3), 0.0);
+    /// assert_eq!(bbox2.overlap(&bbox3), 0.0);
+    /// ```
+    pub fn overlap(&self, other: &BBox) -> f32 {
+        self.intersect_area(other) as f32 / self.union_area(other) as f32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_overlap() {
+        let bbox1 = BBox {
+            x: 0,
+            y: 0,
+            width: 100,
+            height: 100,
+        };
+        let bbox2 = BBox {
+            x: 50,
+            y: 50,
+            width: 100,
+            height: 100,
+        };
+        let bbox3 = BBox {
+            x: 150,
+            y: 150,
+            width: 100,
+            height: 100,
+        };
+        let bbox4 = BBox {
+            x: -50,
+            y: -50,
+            width: 100,
+            height: 100,
+        };
+
+        assert!(bbox1.overlap(&bbox2) > 0.14);
+        assert_eq!(bbox1.overlap(&bbox3), 0.0);
+        assert!(bbox1.overlap(&bbox4) > 0.14);
+        assert_eq!(bbox2.overlap(&bbox3), 0.0);
+        assert_eq!(bbox2.overlap(&bbox4), 0.0);
+        assert_eq!(bbox3.overlap(&bbox4), 0.0);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 #![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
 
+mod bbox;
+
+pub use bbox::BBox;
+
+/// the object-detector-rust prelude
+pub mod prelude {
+    pub use super::bbox::BBox;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
