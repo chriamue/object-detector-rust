e6983a5ab99226f2c3e72de752784fa8d3163ef5
chriamue <chriamue@users.noreply.github.com>
2022-12-29 13:12:59 +0000

    deploy: e2e963b4a8ea085eac77ab0b8472a3ea1c5176ad

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"object_detector_rust":["",[["dataset",[],["folder_dataset.rs","memory_dataset.rs","mod.rs"]],["feature",[],["hog_feature.rs","mod.rs"]]],["bbox.rs","lib.rs","utils.rs"]]\
+"object_detector_rust":["",[["dataset",[],["folder_dataset.rs","memory_dataset.rs","mod.rs"]],["feature",[],["brief_feature.rs","hog_feature.rs","mod.rs"]]],["bbox.rs","lib.rs","utils.rs"]]\
 }');
 createSourceSidebar();
