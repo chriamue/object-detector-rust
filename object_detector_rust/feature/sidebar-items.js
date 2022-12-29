4d0823ff9809d306c722debc439574e5c4038b01
chriamue <chriamue@users.noreply.github.com>
2022-12-29 11:49:24 +0000

    deploy: d5a62af58117a1eb93568eefc16f92365eec3d44

--- object_detector_rust/feature/sidebar-items.js
+++ object_detector_rust/feature/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["DummyFeature","Dummy feature descriptor that always returns the same value"]],"trait":[["Feature","Trait for feature descriptors that can be extracted from images"]]};
+window.SIDEBAR_ITEMS = {"struct":[["DummyFeature","Dummy feature descriptor that always returns the same value"],["HOGFeature","Struct for extracting HOG features from images."]],"trait":[["Feature","Trait for feature descriptors that can be extracted from images"]]};
