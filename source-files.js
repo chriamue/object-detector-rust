b33c543e273bb889366621222181c1e4c554f8f4
chriamue <chriamue@users.noreply.github.com>
2022-12-30 10:56:52 +0000

    deploy: 7d7db37144401bf6409ef1b1844d8a5124c3b134

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"object_detector_rust":["",[["classifier",[],["mod.rs","svm_classifier.rs"]],["dataset",[],["folder_dataset.rs","memory_dataset.rs","mod.rs"]],["feature",[],["brief_feature.rs","hog_feature.rs","mod.rs"]],["utils",[],["mod.rs","sliding_window.rs"]]],["bbox.rs","lib.rs","predictable.rs","trainable.rs"]]\
+"object_detector_rust":["",[["classifier",[],["bayes_classifier.rs","mod.rs","svm_classifier.rs"]],["dataset",[],["folder_dataset.rs","memory_dataset.rs","mod.rs"]],["feature",[],["brief_feature.rs","hog_feature.rs","mod.rs"]],["utils",[],["mod.rs","sliding_window.rs"]]],["bbox.rs","lib.rs","predictable.rs","trainable.rs"]]\
 }');
 createSourceSidebar();
