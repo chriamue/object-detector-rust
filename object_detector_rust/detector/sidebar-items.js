a36b01fc46d56a27e4d0ab5cc1b0cc889605fc7a
chriamue <chriamue@users.noreply.github.com>
2022-12-30 19:21:54 +0000

    deploy: 02c38517f339c807bd1d660b92d68a2832bb9ac4

---
+++ object_detector_rust/detector/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["HOGSVMDetector","Struct representing an object detector using HOG features and an SVM classifier"]],"trait":[["Detector","Trait for object detection"]]};
