8bb8912bdee65ae1e204c79eadd635720a964c16
chriamue <chriamue@users.noreply.github.com>
2022-12-30 09:32:20 +0000

    deploy: 3b53290ad54e6ab890dfef56337c47f4d3376f55

---
+++ implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"object_detector_rust":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.152/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"object_detector_rust/classifier/struct.SVMClassifier.html\" title=\"struct object_detector_rust::classifier::SVMClassifier\">SVMClassifier</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"object_detector_rust":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.152/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"object_detector_rust/classifier/struct.SVMClassifier.html\" title=\"struct object_detector_rust::classifier::SVMClassifier\">SVMClassifier</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
