c6d3d7f6cadfdb69dff6aec27087652b3707bced
Christian M <chriamue@gmail.com>
2022-12-31 12:55:32 +0100

    :white_check_mark: adds default test for hog svm detector

--- src/detector/hog_svm_detector.rs
+++ src/detector/hog_svm_detector.rs
@@ -138,4 +138,31 @@ mod tests {
         assert_eq!(detections[0].bbox, BBox::new(0, 0, 32, 32));
         assert_eq!(detections[0].confidence, 1.0);
     }
+
+    #[test]
+    fn test_default_hog_svm_detector() {
+        // Create a simple image with a single black square
+        let image = DynamicImage::new_luma8(32, 32);
+
+        // Extract HOG features from the image
+        let hog_feature = HOGFeature::new();
+        let hog_features = hog_feature.extract(&image).unwrap();
+
+        // Create training data and labels
+        let x = Array2::from_shape_vec((1, hog_features.len()), hog_features).unwrap();
+        let y = Array1::from(vec![true]);
+
+        // Create a new HOGSVMDetector and train it on the training data
+        let mut hog_svm_detector = HOGSVMDetector::default();
+        hog_svm_detector.fit(&x.view(), &y.view()).unwrap();
+
+        // Make predictions on the image using the trained detector
+        let detections = hog_svm_detector.detect(&image);
+
+        // Check that the predicted detection is correct
+        assert_eq!(detections.len(), 1);
+        assert_eq!(detections[0].class, 1);
+        assert_eq!(detections[0].bbox, BBox::new(0, 0, 32, 32));
+        assert_eq!(detections[0].confidence, 1.0);
+    }
 }
