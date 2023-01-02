5153a17fd3e8c68a04823eb6ca2878075caaedd0
Christian M <chriamue@gmail.com>
2023-01-02 19:37:20 +0100

    :white_check_mark: adds tests for generate negative samples module

--- src/utils/generate_negative_samples.rs
+++ src/utils/generate_negative_samples.rs
@@ -30,7 +30,7 @@ pub fn generate_negative_samples(
         let detections = detector.detect(&annotated_image.image);
         let mut false_pos_annotations = Vec::new();
         detections.iter().for_each(|detection| {
-            if max_annotations.is_some() && max_annotations.unwrap() <= annotations_counter {
+            if max_annotations.is_some() && max_annotations.unwrap() >= annotations_counter {
                 return;
             }
             if detection.class as u32 == class {
@@ -59,4 +59,176 @@ pub fn generate_negative_samples(
         });
     }
     generated_annotated_images
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::{Detection, MemoryDataSet};
+    use crate::BBox;
+
+    use super::*;
+    use image::DynamicImage;
+    use mockall::predicate::*;
+    use mockall::*;
+
+    // Define the mock Detector trait
+    mock! {
+        DummyDetector {}
+        impl Detector for DummyDetector {
+            fn detect(&self, image: &DynamicImage) -> Vec<Detection>;
+        }
+    }
+
+    // Set up the mock Detector with default return values
+    fn mock_detector() -> MockDummyDetector {
+        let mut mock = MockDummyDetector::new();
+        mock.expect_detect().returning(|_| {
+            vec![Detection {
+                bbox: BBox::new(10, 10, 5, 5),
+                class: 1,
+                confidence: 1.0,
+            }]
+        });
+        mock
+    }
+
+    #[test]
+    fn test_generate_negative_samples() {
+        // Set up the mock Detector
+        let mock_detector = mock_detector();
+
+        // Set up the MemoryDataSet and add some test annotated images
+        let mut dataset = MemoryDataSet::new();
+        let annotated_image_1 = AnnotatedImage {
+            image: DynamicImage::new_rgba8(20, 20),
+            annotations: vec![
+                Annotation {
+                    bbox: BBox {
+                        x: 0,
+                        y: 0,
+                        width: 5,
+                        height: 5,
+                    },
+                    class: 0,
+                },
+                Annotation {
+                    bbox: BBox {
+                        x: 5,
+                        y: 5,
+                        width: 5,
+                        height: 5,
+                    },
+                    class: 1,
+                },
+            ],
+        };
+        let annotated_image_2 = AnnotatedImage {
+            image: DynamicImage::new_rgba8(20, 20),
+            annotations: vec![
+                Annotation {
+                    bbox: BBox {
+                        x: 0,
+                        y: 0,
+                        width: 5,
+                        height: 5,
+                    },
+                    class: 1,
+                },
+                Annotation {
+                    bbox: BBox {
+                        x: 10,
+                        y: 10,
+                        width: 10,
+                        height: 10,
+                    },
+                    class: 1,
+                },
+            ],
+        };
+        dataset.add_annotated_image(annotated_image_1);
+        dataset.add_annotated_image(annotated_image_2);
+
+        // Call generate_negative_samples with the mock Detector and test dataset
+        let negative_samples = generate_negative_samples(&dataset, &mock_detector, 1, None);
+
+        // Verify that the correct number of negative samples were generated
+        assert_eq!(negative_samples.len(), 2);
+        assert_eq!(negative_samples[0].annotations.len(), 1);
+        assert_eq!(negative_samples[1].annotations.len(), 0);
+    }
+
+    #[test]
+    fn test_add_hard_negative_samples() {
+        // Set up the mock Detector
+        let mock_detector = mock_detector();
+
+        // Set up the MemoryDataSet and add some test annotated images
+        let mut dataset = MemoryDataSet::new();
+        let annotated_image_1 = AnnotatedImage {
+            image: DynamicImage::new_rgba8(20, 20),
+            annotations: vec![
+                Annotation {
+                    bbox: BBox {
+                        x: 0,
+                        y: 0,
+                        width: 5,
+                        height: 5,
+                    },
+                    class: 0,
+                },
+                Annotation {
+                    bbox: BBox {
+                        x: 5,
+                        y: 5,
+                        width: 5,
+                        height: 5,
+                    },
+                    class: 1,
+                },
+            ],
+        };
+        let annotated_image_2 = AnnotatedImage {
+            image: DynamicImage::new_rgba8(20, 20),
+            annotations: vec![
+                Annotation {
+                    bbox: BBox {
+                        x: 0,
+                        y: 0,
+                        width: 5,
+                        height: 5,
+                    },
+                    class: 1,
+                },
+                Annotation {
+                    bbox: BBox {
+                        x: 10,
+                        y: 10,
+                        width: 10,
+                        height: 10,
+                    },
+                    class: 1,
+                },
+            ],
+        };
+        dataset.add_annotated_image(annotated_image_1);
+        dataset.add_annotated_image(annotated_image_2);
+
+        // Call generate_negative_samples with the mock Detector and test dataset
+        add_hard_negative_samples(&mut dataset, &mock_detector, 1, None);
+        assert_eq!(dataset.annotated_images_len(), 4);
+        assert_eq!(
+            dataset
+                .annotated_images()
+                .skip(2)
+                .next()
+                .unwrap()
+                .annotations
+                .len(),
+            1
+        );
+        assert_eq!(
+            dataset.annotated_images().last().unwrap().annotations.len(),
+            0
+        );
+    }
 }
