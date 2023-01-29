80e68f5e4f9f12f9c27a990100377f8c6f0189f0
Christian M <chriamue@gmail.com>
2023-01-29 13:05:53 +0100

    :bug: prevent sliding window overflow

--- src/utils/sliding_window.rs
+++ src/utils/sliding_window.rs
@@ -20,8 +20,18 @@ impl WindowGenerator<DynamicImage> for SlidingWindow {
     ) -> Vec<ImageWindow<SubImage<&'b DynamicImage>>> {
         let mut windows = Vec::new();
         let (width, height) = image.dimensions();
-        for y in (0..height).step_by(self.step_size as usize) {
-            for x in (0..width).step_by(self.step_size as usize) {
+        let y_border = if self.height - self.step_size > 0 {
+            self.height
+        } else {
+            0
+        };
+        let x_border = if self.width - self.step_size > 0 {
+            self.width
+        } else {
+            0
+        };
+        for y in (0..height.saturating_sub(y_border)).step_by(self.step_size as usize) {
+            for x in (0..width.saturating_sub(x_border)).step_by(self.step_size as usize) {
                 let window = ImageWindow {
                     x,
                     y,
@@ -57,9 +67,35 @@ mod tests {
 
         // Check that the positions and subimages of the returned windows are correct
         assert_eq!(windows[0].x, 0);
+        assert_eq!(windows[0].x, 0);
         assert_eq!(windows[1].x, 5);
         assert_eq!(windows[2].y, 5);
         assert_eq!(windows[3].x, 5);
+        assert_eq!(windows[3].y, 5);
+    }
+
+    #[test]
+    fn test_sliding_window_step_size() {
+        let image = DynamicImage::new_rgb8(21, 21);
+        let window_size = 10;
+        let step_size = 5;
+
+        let generator = SlidingWindow {
+            width: window_size,
+            height: window_size,
+            step_size: step_size,
+        };
+
+        let windows = generator.windows(&image);
+
+        // Check that the correct number of windows was returned
+        assert_eq!(windows.len(), 9);
+
+        // Check that the positions and subimages of the returned windows are correct
+        assert_eq!(windows[0].x, 0);
+        assert_eq!(windows[1].x, 5);
+        assert_eq!(windows[2].y, 0);
+        assert_eq!(windows[3].x, 0);
         assert_eq!(windows[3].y, 5);
     }
 }
