474589e16933a301c52e8b31c67acc088737649f
chriamue <chriamue@users.noreply.github.com>
2023-02-11 13:28:38 +0000

    deploy: 76fc1440fbd138aa8d555ce028ffac03be48f438

--- object_detector_rust/window_generator/sidebar-items.js
+++ object_detector_rust/window_generator/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["SlidingWindow","Struct for generating sliding windows over an image"]],"trait":[["WindowGenerator","Trait for generating windows over an image"]]};
+window.SIDEBAR_ITEMS = {"struct":[["PyramidWindow","Struct for generating sliding windows over an image"],["SlidingWindow","Struct for generating sliding windows over an image"]],"trait":[["WindowGenerator","Trait for generating windows over an image"]]};
